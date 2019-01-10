1ff49e3d30643de31841298dbec70a74d26119f5
unknown <mauro.kuepfer@bluewin.ch>
2019-01-10 10:12:04 +0100

    implement default calendar view

--- MIRQ57-Frontend/src/app/app.module.ts
+++ MIRQ57-Frontend/src/app/app.module.ts
@@ -10,6 +10,13 @@ import { EventViewComponent } from './event-view/event-view.component';
 import { EventEditComponent } from './event-edit/event-edit.component';
 import { EventParticipateComponent } from './event-participate/event-participate.component';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +29,15 @@ import { EventParticipateComponent } from './event-participate/event-participate
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
