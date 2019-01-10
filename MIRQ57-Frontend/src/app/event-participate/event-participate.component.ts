44136e9a427a2df7af1ca50877d9daef239ee3d0
unknown <mauro.kuepfer@bluewin.ch>
2019-01-10 09:23:56 +0100

    add file structure

---
+++ MIRQ57-Frontend/src/app/event-participate/event-participate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-participate',
+  templateUrl: './event-participate.component.html',
+  styleUrls: ['./event-participate.component.css']
+})
+export class EventParticipateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
