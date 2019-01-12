e17d88fad1aabbdf4caa8f1b472e1cdb77b05fb3
unknown <mauro.kuepfer@bluewin.ch>
2019-01-12 13:25:22 +0100

    add routes for view components

--- MIRQ57-Frontend/src/app/app-routing.module.ts
+++ MIRQ57-Frontend/src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 import { CalendarOverviewComponent } from './calendar-overview/calendar-overview.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { EventParticipateComponent } from './event-participate/event-participate.component';
+import { EventViewComponent } from './event-view/event-view.component';
+import { EventEditComponent } from './event-edit/event-edit.component';
+import { EventCreateComponent } from './event-create/event-create.component';
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'calendar', component: CalendarOverviewComponent },
+  { path: 'event/participate', component: EventParticipateComponent },
+  { path: 'event/view', component: EventViewComponent },
+  { path: 'event/edit', component: EventEditComponent },
+  { path: 'event/create', component: EventCreateComponent },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
